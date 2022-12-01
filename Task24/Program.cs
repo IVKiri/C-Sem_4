3eb661c679b3df66c01a5ee8a99e20f75c02bafc
Inna Kirianova <innr@ya.ru>
2022-12-02 00:49:45 +0300

    added code for task 24

---
+++ Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// Console.Write("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// int result = 0;
+// for (int i = 1; i <= A; i++) 
+// {
+//     result = result + i;
+// }
+// Console.WriteLine(result);
+
+int Sum(int number) {
+    int result = 0;
+    for (int i = 1; i <= number; i++) {
+        result = result + i;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int result = Sum(A);
+Console.WriteLine(result);
