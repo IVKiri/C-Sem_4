a883d0af24c973950f63a97b03e2817d6deb8711
Inna Kirianova <innr@ya.ru>
2022-12-02 23:12:45 +0300

    added code for task 28

---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int Prod(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+Console.Write("Введите чсло: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int production = Prod(N);
+Console.WriteLine(production);
