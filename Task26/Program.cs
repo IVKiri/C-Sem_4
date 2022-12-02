66798836ca7034d3ac716dcbdd6e109936d6e2ad
Inna Kirianova <innr@ya.ru>
2022-12-02 23:12:30 +0300

    added code for task 26

---
+++ Task26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+for (int i = 0; number > 0; i++)
+{
+    count++;
+    number = number / 10;
+}
+Console.WriteLine("Количество знаков: " + count);
+
